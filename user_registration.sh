dce9235f18079f30bd0bb2626d05ceb30952efce
Ankita <Ankitaangane20@gmail.com>
2020-10-21 22:55:44 +0530

    Added email address and its validation

--- user_registration.sh
+++ user_registration.sh
@@ -3,7 +3,12 @@
 #Author: Ankita Angane
 #User Registration Program and validation using regex
 
+shopt -s extglob
+
 regex="^[A-Z]{1}[a-z]{2,}"
+#regexMail="^[A-Za-z0-9]+([._+-][A-Za-z0-9]+)*@[A-Za-z0-9]+.[A-Za-z]{2,4}([.][A-Za-z]{2})$"
+#regexMail="^([A-Za-z0-9+._-]*[A-Za-z0-9])+@[A-Za-z0-9]+.[A-Za-z]{2,}$"
+regexMail="^([A-Za-z0-9+._-]*[A-Za-z0-9])+@[A-Za-z0-9]+[.A-Za-z]*[.A-Za-z]$"
 
 read -p "Enter a first name - " name
 
@@ -14,6 +19,7 @@ else
 	echo "Invalid Name"
 fi
 
+echo ""
 read -p "Enter a last name - " surname
 
 if [[ $surname =~ $regex ]]
@@ -23,4 +29,12 @@ else
         echo "Invalid Surname"
 fi
 
+echo ""
+read -p "Enter a Email Address - " mail
 
+if [[ $mail =~ $regexMail ]]
+then
+        echo "Valid Mail"
+else
+        echo "Invalid Mail"
+fi
